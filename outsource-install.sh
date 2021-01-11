b9ca5d730c0f3ca7e7155d20155737d73a08764f
90zlaya <contact@zlatanstajic.com>
2021-01-11 20:33:49 +0100

    outsource-install.sh :green_heart: refactoring

--- outsource-install.sh
+++ outsource-install.sh
@@ -2,46 +2,94 @@
 
 ################################################################################
 # Script name : outsource-install.sh
-# Description : Install necessary PHP extensions for PHP Library
-# Arguments   : MAIN_VERSION (optional)
+# Description : Install outsource folder for PHP Library
+# Arguments   : main-version (optional)
 # Author      : 90zlaya
 # Email       : contact@zlatanstajic.com
 # Licence     : MIT
 ################################################################################
 
 ################################################################################
-
 # Globals
+################################################################################
+
+SCRIPT_NAME="`basename $(readlink -f $0)`"
+SCRIPT_DIR="`dirname $(readlink -f $0)`"
 MAIN_VERSION="1.1.0"
 FOLDER_NAME="outsource"
 ARCHIVE_NAME="archive"
 
 ################################################################################
+# Show help
+################################################################################
 
-# Switch main version if first param is set
-if [ -n "$1" ]
-then
-  echo -e "Overriding main version to $1"
-  MAIN_VERSION=$1
-fi
+Help()
+{
+  echo ""
+  echo -e "\e[1mRunning $SCRIPT_NAME\e[0m"
+  echo "Description: Install outsource folder for PHP Library"
+  echo ""
+  echo "Show this help                   : $SCRIPT_NAME -h"
+  echo "Install default outsource folder : $SCRIPT_NAME"
+  echo "Install version outsource folder : $SCRIPT_NAME <main-version>"
+  echo ""
+}
 
-# Download outsource repository
+################################################################################
+# Getting parameters
+################################################################################
+
+GetParameters()
+{
+  if [ $# -eq 1 ]
+  then
+    if [ "x$1" = "x-h" ]
+    then
+      Help
+      End 0
+    else
+      # Switch main version
+      echo -e "Overriding main version to $1"
+      MAIN_VERSION=$1
+    fi
+  fi
+}
+
+################################################################################
+# Shell terminates
+################################################################################
+
+End()
+{
+  if [ $1 -eq 0 ]
+  then
+    echo "Script $SCRIPT_NAME finishing OK"
+    echo ""
+    exit 0
+  else
+    echo -e "Script $SCRIPT_NAME finishing with \e[1mERROR [$2]\e[0m"
+    echo ""
+    exit 1
+  fi
+}
+
+################################################################################
+# Executing all
+################################################################################
+
+echo ""
+echo "Script $SCRIPT_NAME starting..."
+
+GetParameters $@
+
 wget "https://github.com/php-library-league/${FOLDER_NAME}/${ARCHIVE_NAME}/${MAIN_VERSION}.zip"
-
-# Unzip and remove downloaded file
 unzip "${MAIN_VERSION}.zip" && rm -rf "${MAIN_VERSION}.zip"
-
-# Rename folder
 mv "${FOLDER_NAME}-${MAIN_VERSION}/" "${FOLDER_NAME}/"
-
-# Unzip archive
 unzip "${FOLDER_NAME}/${ARCHIVE_NAME}.zip" -d "${FOLDER_NAME}/"
-
-# Give full permissions to all outsource folders and files
 chmod -R 0777 "${FOLDER_NAME}/"
-
-# Update Composer vendors folder
 composer update
-
-# Run PHPUnit tests
 composer run phpunit
+
+End 0
+
+################################################################################
